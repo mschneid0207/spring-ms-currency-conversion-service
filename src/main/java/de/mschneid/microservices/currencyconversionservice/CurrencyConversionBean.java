9fb7813dd2dfecac0f8401bdc6087ac9987e0606
Markus Schneider <markus.schneider0207@gmail.com>
2018-09-07 12:46:20 +0200

    created currency conversion service

---
+++ src/main/java/de/mschneid/microservices/currencyconversionservice/CurrencyConversionBean.java
@@ -0,0 +1,22 @@
+package de.mschneid.microservices.currencyconversionservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyConversionBean {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private int port;
+}
